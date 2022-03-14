12c07896f135368c8864718bf1cf9ab170749e78
StrikebyClaymore <36325557+StrikebyClaymore@users.noreply.github.com>
2022-03-14 15:09:45 +0300

    SpawnChangeConfig

---
+++ Assets/Scripts/SpawnChance.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SpawnChanceConfig", menuName = "SpawnChance", order = 51)]
+public class SpawnChance : ScriptableObject
+{
+    public int apple_chance;
+    public int knife_chance;
+}
