a16b4c3cb2ea7a9d600ee9b1fa68fcc1a616d612
StrikebyClaymore <36325557+StrikebyClaymore@users.noreply.github.com>
2022-03-14 15:09:05 +0300

    Log

--- Assets/Scripts/Log.cs
+++ Assets/Scripts/Log.cs
@@ -6,9 +6,18 @@ using UnityEngine;
 public class Log : MonoBehaviour
 {
     [SerializeField] private float rotateSpeed = 1.5f;
+    [SerializeField] private int hp = 5; 
     
     private void FixedUpdate()
     {
         transform.Rotate(0, 0, rotateSpeed);
+    }
+
+    public void GetHit()
+    {
+        hp = Mathf.Max(0, hp - 1);
+        if (hp == 0)
+            Destroy(gameObject);
+        GameManager.LevelManager.LevelCompleted();
     }
 }
