e413aa698654c7a724e9b4401f0c467906468047
StrikebyClaymore <36325557+StrikebyClaymore@users.noreply.github.com>
2022-03-14 15:08:13 +0300

    Apple

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    [SerializeField] private GameObject part1;
+    [SerializeField] private GameObject part2;
+    [SerializeField] private float force = 4f;
+    public void GetHit()
+    {
+        part1.transform.SetParent(transform.parent.parent);
+        part2.transform.SetParent(transform.parent.parent);
+        part1.SetActive(true);
+        part2.SetActive(true);
+        var angle = Random.Range(10, 30);
+        var rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        var direction = rotation * Vector3.up;
+        part1.GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
+        part1.GetComponent<Rigidbody2D>().AddTorque(1, ForceMode2D.Force);
+        angle = Random.Range(-10, -30);
+        rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        direction = rotation * Vector3.up;
+        part2.GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
+        part2.GetComponent<Rigidbody2D>().AddTorque(1, ForceMode2D.Force);
+
+        GameManager.GameData.Apples++;
+    }
+}
--- Assets/Scripts/AppleSpawnChance.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "AppleSpawnChanceConfig", menuName = "AppleSpawnChance", order = 51)]
-public class AppleSpawnChance : ScriptableObject
-{
-    public int chance;
-}
