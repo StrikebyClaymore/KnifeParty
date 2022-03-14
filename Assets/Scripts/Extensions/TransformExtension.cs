ba8984846b0cb65bec136824739c35cec613d064
StrikebyClaymore <36325557+StrikebyClaymore@users.noreply.github.com>
2022-03-14 18:18:44 +0300

    Transform extension

---
+++ Assets/Scripts/Extensions/TransformExtension.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Extensions
+{
+    public static class TransformExtension
+    {
+        public static Transform Clear(this Transform transform)
+        {
+            foreach (Transform child in transform)
+            {
+                Object.Destroy(child.gameObject);
+            }
+            return transform;
+        }
+    }
+}
