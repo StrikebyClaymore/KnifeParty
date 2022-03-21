b3ad3906de25e09dd17f224daa48f7292863ffe3
StrikebyClaymore <36325557+StrikebyClaymore@users.noreply.github.com>
2022-03-21 19:16:38 +0300

    Target rotation speed change

--- Assets/Scripts/Target/TargetRotation.cs
+++ Assets/Scripts/Target/TargetRotation.cs
@@ -1,14 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TargetRotation : MonoBehaviour
 {
+    [SerializeField] private float maxRotateSpeed = 4.0f;
+    [SerializeField] private float minRotateSpeed = 1.0f;
     [SerializeField] private float rotateSpeed = 1.5f;
-    
-    private void FixedUpdate()
+
+    private int _speedDirection = 1;
+    private int _moveDirection = 1;
+
+    private void Update()
     {
-        transform.Rotate(0, 0, rotateSpeed);
+        SimpleDirectonChanger();
     }
 
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, 0, _moveDirection * rotateSpeed);
+    }
+
+    private void SimpleDirectonChanger()
+    {
+        rotateSpeed += _speedDirection * Time.deltaTime;
+        if (rotateSpeed >= maxRotateSpeed)
+            _speedDirection *= -1;
+        if (rotateSpeed <= minRotateSpeed)
+        {
+            _speedDirection *= -1;
+            _moveDirection *= -1;
+        }
+    }
 }
